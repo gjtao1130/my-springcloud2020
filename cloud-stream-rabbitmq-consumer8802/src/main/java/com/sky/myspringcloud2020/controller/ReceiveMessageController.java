214755657dc4b853ec71e6bf8b1de65d8bc3ffca
gjtao1130 <gjtao1130@163.com>
2020-04-13 15:15:04 +0800

    my-springcloud2020-20200413

---
+++ cloud-stream-rabbitmq-consumer8802/src/main/java/com/sky/myspringcloud2020/controller/ReceiveMessageController.java
@@ -0,0 +1,23 @@
+package com.sky.myspringcloud2020.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.messaging.Message;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableBinding(Sink.class)
+public class ReceiveMessageController {
+
+    @Value("${server.port}")
+    private String port;
+
+    @GetMapping("/getMsg")
+    @StreamListener(Sink.INPUT)
+    public void input(Message<String> message){
+        System.out.println("***1: "+message.getPayload()+" ***port: "+port);
+    }
+}
